53275e3d5ce8d4000ae97b27f65261cb25d438c4
Victor Gabriel Aires da Silva <gabriel_aires@estudante.sesisenai.org.br>
2025-08-05 21:33:43 -0300

    Create funcao.c

---
+++ funcao.c
@@ -0,0 +1,12 @@
+// exemplo 1
+#include <stdio.h>
+ int soma2 (int a,int b){
+   return a + b;
+ } 
+int main() {
+   
+   int resul = soma2(1, 2);
+   printf("Hello Word %d %d %d %d", resul +1, resul, resul, resul);
+    return 0;
+    
+}

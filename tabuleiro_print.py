620946c45d3882e82a772a249b808a57c8dcab09
Victor Gabriel Aires da Silva <gabriel_aires@estudante.sesisenai.org.br>
2025-08-12 21:00:41 -0300

    Update tabuleiro_print.py

--- tabuleiro_print.py
+++ tabuleiro_print.py
@@ -5,10 +5,10 @@ tab = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 def print_tab(tab): #cria função
     os.system('cls') #limpa a tela
 
-    print(" " + tab[0], tab[1], tab[2], sep="  |")
+    print("  " + tab[0], tab[1], tab[2], sep="  | ")
     print("+---+---+---+")
-    print(" " + tab[3], tab[4], tab[5], sep="  |")
+    print("  " + tab[3], tab[4], tab[5], sep="  | ")
     print("+---+---+---+")
-    print(" " + tab[6], tab[7], tab[8], sep="  |")
+    print("  " + tab[6], tab[7], tab[8], sep="  | ")
     
 print_tab(tab) #retorna a função
